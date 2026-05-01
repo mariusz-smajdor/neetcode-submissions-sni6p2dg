95a2190b69c4ee8e12c3f107ade600c5a990beed
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2026-05-01 03:44:44 +0200

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isSubsequence(s, t) {
+        let j = 0;
+        for (let i = 0; i < t.length; i++) {
+            if (t[i] === s[j]) {
+                j++
+            }
+        }
+
+        if (j === s.length) {
+            return true
+        }
+        return false
+    }
+}
