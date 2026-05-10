ab906b6d3a970d5038fc7605bbfc107580979df4
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2026-05-10 20:47:32 +0200

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const map = {}
+
+        for (let i = 0; i < nums.length; i++) {
+            const diff = target - nums[i]
+
+            if (diff in map) {
+                return [map[diff], i]
+            }
+            map[nums[i]] = i
+        }
+        return []
+    }
+}
+
+
