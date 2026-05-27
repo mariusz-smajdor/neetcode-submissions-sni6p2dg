8e598f80a1a3e4a26ebdeec6fe392add4662dbea
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2026-05-27 20:34:35 +0200

    Add: range-sum-query-immutable - submission-1

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-1.js
@@ -0,0 +1,21 @@
+class NumArray {
+    nums
+    currRange = 1
+    /**
+     * @param {number[]} nums
+     */
+    constructor(nums) {
+        this.nums = nums
+    }
+
+    /**
+     * @param {number} left
+     * @param {number} right
+     * @return {number}
+     */
+    sumRange(left, right) {
+        return this.nums.slice(left, right + 1).reduce((acc, curr) => acc + curr, 0)
+    }
+
+
+}
